1bf5b6cc81580627210c6631fab31b3bdc5628c4
Roland Kujundzic <roland@inkoeln.com>
2018-04-24 15:13:04 +0200

    nginx added

--- debian/run.sh
+++ debian/run.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-START_SERVICE="ssh mysql"
+#------------------------------------------------------------------------------
+function _abort {
+	echo -e "\nABORT: $1\n\n"
+	exit 1
+}
+
+
+#------------------------------------------------------------------------------
+function _check_is_running {
+	if test -f /etc/init.d/$1; then 
+		IS_RUNNING=`/etc/init.d/$1 status | grep "$2"` 
+
+		if test -z "$IS_RUNNING"; then
+			_abort "$1 is down"
+		fi
+	fi
+}
+
+
+
+#
+# M A I N
+#
+
+START_SERVICE="ssh mysql nginx"
+STATUS_IS_RUNNING="ssh nginx"
 
 for a in $START_SERVICE; do 
 	if test -f /etc/init.d/$a; then 
@@ -11,15 +36,11 @@ done
 mysql-create "$DB_NAME" "$DB_PASS"
 
 while sleep 60; do
-	for a in $START_SERVICE; do 
-		if test -f /etc/init.d/$a; then 
-			STATUS=`/etc/init.d/$a status` # | grep Uptime (if mysql) 
 
-			if test -z "$STATUS"; then
-				echo "$a is down"
-				exit 1
-			fi
-		fi
+	for a in $STATUS_IS_RUNNING; do 
+		_check_is_running $a "$a is running"
 	done
+
+	_check_is_running mysql "Uptime"
 done
 
