c6c1105ff15ceaefafbc99488a90686fa54ea5fb
Roland Kujundzic <roland@kujundzic.de>
2018-10-07 17:26:35 +0200

    update

--- ubuntu/bionic/openvpn/sbin/docker_entrypoint.sh
+++ ubuntu/bionic/openvpn/sbin/docker_entrypoint.sh
@@ -5,4 +5,11 @@ if ! test -z "$VPN_LOGIN" && ! test -z "$VPN_PASSWORD"; then
 	openvpn /etc/openvpn/client/Germany1.ovpn
 fi
 
+HAS_NET=`ping -c 1 heise.de 2> /dev/null | grep '64 bytes from'`
+if test -z "$HAS_NET"; then
+	echo -e "search google.com\nnameserver 8.8.8.8\nnameserver 8.8.4.4" > /etc/resolv.conf
+fi
+
+cd /webhome
+
 exec "$@"
