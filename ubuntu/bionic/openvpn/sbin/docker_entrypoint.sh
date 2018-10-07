627d5e76ac7b7ff2e9f74ca12d3dc3c7019491d4
Roland Kujundzic <roland@kujundzic.de>
2018-10-07 17:07:46 +0200

    start openvpn

--- ubuntu/bionic/openvpn/sbin/docker_entrypoint.sh
+++ ubuntu/bionic/openvpn/sbin/docker_entrypoint.sh
@@ -2,6 +2,7 @@
 
 if ! test -z "$VPN_LOGIN" && ! test -z "$VPN_PASSWORD"; then
 	echo -e "$VPN_LOGIN\n$VPN_PASSWORD" > /etc/openvpn/client/login.conf
+	openvpn /etc/openvpn/client/Germany1.ovpn
 fi
 
 exec "$@"
