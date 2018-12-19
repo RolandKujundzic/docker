91d8793b6bb889f4d4f693130a154863e8890467
Roland Kujundzic <roland@inkoeln.com>
2018-12-19 14:26:35 +0100

    use /webhome/app as workdir

--- ubuntu/bionic/openvpn.sh
+++ ubuntu/bionic/openvpn.sh
@@ -2,4 +2,4 @@ DOCKER_IMAGE=rk_bionic:openvpn
 DOCKER_DF=openvpn.df
 DOCKER_PARAMETER="--privileged -e VPN_LOGIN=*** -e VPN_PASSWORD=***"
 DOCROOT_SOURCE=/webhome/***
-DOCROOT_TARGET=/webhome
+DOCROOT_TARGET=/webhome/app
--- ubuntu/bionic/webserver.sh
+++ ubuntu/bionic/webserver.sh
@@ -1,2 +1,5 @@
 DOCKER_IMAGE=rk_bionic:webserver
 DOCKER_DF=webserver.df
+DOCROOT_SOURCE=/webhome/***
+DOCROOT_TARGET=/webhome/app
+
