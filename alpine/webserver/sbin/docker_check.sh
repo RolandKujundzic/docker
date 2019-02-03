627175264f75b727b7b3ab4c85a71c9426fa6206
Roland Kujundzic <roland@inkoeln.com>
2019-02-03 17:47:27 +0100

    update

--- alpine/webserver/sbin/docker_check.sh
+++ alpine/webserver/sbin/docker_check.sh
@@ -1,40 +1,88 @@
 #!/bin/ash
 
 #------------------------------------------------------------------------------
+function _log {
+  echo "$1" > /proc/self/fd/1
+
+  if test -f "/webhome/app/data/docker/docker.log"; then
+    echo "$1" >> "/webhome/app/data/docker/docker.log"
+  fi
+}
+
+
+#------------------------------------------------------------------------------
 function _check {
+  local TS=`date +'%Y-%m-%d %H:%M:%S'`
   local RE=
+
+  _log "[$TS] check $1 (pid=$2) (port=$3)"
 
   if ! test -z "$2"; then
     if test -f "$2"; then
       local MY_PID=`cat "$2"`
       RE="[${MY_PID:0:1}]${MY_PID:1}"
     else
-      echo "PID File $2 of $1 missing"
+      _log "[$TS] PID File $2 of $1 missing - restart $1"
+      _restart $1
     fi
   else
     RE="[${1:0:1}]${1:1}"
   fi
 
-  local IS_RUNNING=
   if ! test -z "$RE"; then
-    echo "grep $RE in ps aux"
-    IS_RUNNING=`ps aux | grep -E "$RE"`
+    local IS_RUNNING=`ps aux | grep -E "$RE"`
+
+    if test -z "$IS_RUNNING"; then
+      _log "[$TS] ps aux $1 failed - restart $1"
+      _restart_$1
+    fi
   fi
 
-  if test -z "$IS_RUNNING"; then
-		local TS=`date +'%Y-%m-%d %H:%M:%S'`
-    echo "$1 is not running ($TS) - try restart: $3"
-		$3
+  if ! test -z "$3"; then
+    local IS_OPEN=`nc -z -v -w5 localhost "$3" 2>&1 > /dev/null | grep 'open'`
+
+    if test -z "$IS_OPEN"; then
+      _log "[$TS] $1 port $3 is not open - restart $1"
+      _restart_$1
+    fi
+  fi
+
+  local HAS_LSOF=`lsof -n "$1"`
+  if test -z "$HAS_LSOF"; then
+    _log "[$TS] lsof -n $1 failed - restart $1"
+    _restart_$1
   fi
 }
+
+
+#------------------------------------------------------------------------------
+function _restart_sshd {
+  /usr/sbin/sshd
+  sleep 5
+}
+
+
+#------------------------------------------------------------------------------
+function _restart_mysqld {
+  /usr/bin/mysqld_safe
+  sleep 10
+}
+
+
+#------------------------------------------------------------------------------
+function _restart_httpd {
+  /usr/sbin/httpd -k start
+  sleep 10
+}
+
 
 #------------------------------------------------------------------------------
 # M A I N
 #------------------------------------------------------------------------------
 
 while sleep 60; do
-  _check "sshd" "/var/run/sshd.pid" "/usr/sbin/sshd"
-  _check "mysqld" "/var/run/mysqld/mysqld.pid" "/usr/bin/mysqld_safe"
-  _check "httpd" "" "httpd -D FOREGROUND"
+  _check "sshd" "/var/run/sshd.pid" 22
+  _check "mysqld" "/var/run/mysqld/mysqld.pid" 
+  _check "httpd" "" 80
 done
 
--- alpine/webserver/sbin/docker_entrypoint.sh
+++ alpine/webserver/sbin/docker_entrypoint.sh
@@ -1,24 +1,72 @@
 #!/bin/ash
 
+#------------------------------------------------------------------------------
+function _log {
+	echo "$1"
+
+	if ! test -z "$DOCKER_LOGDIR" && test -f "$DOCKER_LOGDIR/docker.log"; then
+		echo "$1" >> "$DOCKER_LOGDIR/docker.log"
+  fi
+}
+
+
+#------------------------------------------------------------------------------
+function _docker_log {
+
+	if ! test -d /webhome/app/data; then
+		return
+	fi
+
+	DOCKER_LOGDIR="/webhome/app/data/docker"
+
+  if ! test -d "$DOCKER_LOGDIR"; then
+    mkdir -p "$DOCKER_LOGDIR"
+  fi
+
+  if ! test -f "$DOCKER_LOGDIR/docker.log"; then
+    local TS=`date +'%Y-%m-%d %H:%M:%S'`
+    echo "$TS" > "$DOCKER_LOGDIR/docker.log"
+  fi
+
+  if ! test -L "$DOCKER_LOGDIR/apache_error.log"; then
+    ln -sf "$DOCKER_LOGDIR/apache_error.log" "/var/log/apache2/error.log"
+  fi
+
+  if ! test -L /webhome/app/data/docker/apache_access.log; then
+    ln -sf "$DOCKER_LOGDIR/apache_access.log" "/var/log/apache2/access.log"
+  fi
+}
+
+
+#------------------------------------------------------------------------------
+# M A I N
+#------------------------------------------------------------------------------
+
+_docker_log
+
 if ! test -z "$SET_UID" && ! test "$SET_UID" = "1000"; then
+	_log "usermod -u $SET_UID rk"
 	usermod -u $SET_UID rk
 fi
 
 if ! test -z "$SET_GID" && ! test "$SET_GID" = "1000"; then
+	_log "groupmod -g $SET_GID rk"
 	groupmod -g $SET_GID rk
 fi
 
-
 if ! test -f /etc/ssh/ssh_host_rsa_key; then
+	_log "ssh-keygen -A"
 	ssh-keygen -A
 fi
 
-# start sshd
+_log "start sshd: /usr/sbin/sshd"
 /usr/sbin/sshd 
 
 if ! test -d /var/lib/mysql/mysql; then
+	_log "initialize mysql: mysql_install_db --user=mysql"
 	mysql_install_db --user=mysql > /dev/null
 
+	_log "prepare: /run/mysqld"
 	if ! test -d "/run/mysqld"; then
 		mkdir -p /run/mysqld
 	fi
@@ -27,18 +75,18 @@ if ! test -d /var/lib/mysql/mysql; then
 
 	if test -z "$MYSQL_ROOT_PASS"; then
 		MYSQL_ROOT_PASS=`pwgen 16 1`
-		echo "[i] MySQL root Password: $MYSQL_ROOT_PASS"
+		_log "[i] MySQL root Password: $MYSQL_ROOT_PASS"
 	fi
 
 	CREATE_SQL_ADMIN=
 	if ! test -z "$SQL_PASS"; then
-		echo "[i] Create MySQL Administrator sql:$SQL_PASS"
+		_log "[i] Create MySQL Administrator sql:$SQL_PASS"
 		CREATE_SQL_ADMIN="GRANT ALL PRIVILEGES ON *.* TO 'sql'@'localhost' IDENTIFIED BY '$SQL_PASS' WITH GRANT OPTION;"
 	fi
 
 	CREATE_DB=
 	if ! test -z "$DB_NAME" && ! test -z "$DB_PASS"; then
-		echo "[i] Create MySQL Database $DB_NAME and account $DB_NAME:$DB_PASS"
+		_log "[i] Create MySQL Database $DB_NAME and account $DB_NAME:$DB_PASS"
 		CREATE_DB="CREATE DATABASE `$DB_NAME`; GRANT ALL PRIVILEGES ON `$DB_NAME`.* TO '$DB_NAME'@'localhost' IDENTIFIED BY '$DB_PASS';"
 	fi
 
@@ -53,15 +101,16 @@ FLUSH PRIVILEGES;
 $CREATE_DB
 EOF
 
+	_log "/usr/bin/mysqld --bootstrap --verbose=1 --skip-name-resolve < $TFILE"
 	/usr/bin/mysqld --bootstrap --verbose=1 --skip-name-resolve < $TFILE
 fi
 
-# start mysqld
+_log "start mysqld: /usr/bin/mysqld_safe"
 /usr/bin/mysqld_safe
 
-# check processes ... kill container if one is down
-/sbin/docker_check.sh &
+_log "start apache2: /usr/sbin/httpd -k start"
+/usr/sbin/httpd -k start
 
-echo "start: $@"
+_log "exec: $@"
 exec "$@"
 
