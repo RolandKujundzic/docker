cdcad84899121619af72e566b5be3664c7be92d0
Roland Kujundzic <roland@inkoeln.com>
2019-02-03 12:09:26 +0100

    update

--- alpine/webserver/sbin/docker_entrypoint.sh
+++ alpine/webserver/sbin/docker_entrypoint.sh
@@ -1,44 +1,54 @@
 #!/bin/bash
 
-if test -d /webhome/rk; then
-	if ! test -z "$SET_UID"; then
-		usermod -u $SET_UID rk
-	fi
-
-	if ! test -z "$SET_GID"; then
-		groupmod -g $SET_GID rk
-	fi
+if ! test -z "$SET_UID"; then
+	usermod -u $SET_UID rk
 fi
 
-if test -f /etc/init.d/mysql; then
-	if ! test -d /var/lib/mysql/mysql; then
-		CREATE_MYSQL_ACCOUNT=y
-		mysql_install_db
+if ! test -z "$SET_GID"; then
+	groupmod -g $SET_GID rk
+fi
+
+if ! test -d /var/lib/mysql/mysql; then
+	mysql_install_db --user=mysql > /dev/null
+  chown -R mysql.mysql /var/lib/mysql
+
+	if ! test -d "/run/mysqld"; then
+		mkdir -p /run/mysqld
 	fi
 
-	service mysql start
+	chown -R mysql:mysql /run/mysqld
 
+	if test -z "$MYSQL_ROOT_PASS"; then
+		MYSQL_ROOT_PASS=`pwgen 16 1`
+		echo "[i] MySQL root Password: $MYSQL_ROOT_PASS"
+	fi
+
+	CREATE_SQL_ADMIN=
 	if ! test -z "$SQL_PASS"; then
 		# create mysql administrator account sql:$SQL_PASS
-		echo "GRANT ALL PRIVILEGES ON *.* TO 'sql'@'localhost' IDENTIFIED BY '$SQL_PASS' WITH GRANT OPTION" | mysql -u root
+		CREATE_SQL_ADMIN="echo \"GRANT ALL PRIVILEGES ON *.* TO 'sql'@'localhost' IDENTIFIED BY '$SQL_PASS' WITH GRANT OPTION;\""
 	fi
 
+	CREATE_DB=
 	if ! test -z "$DB_NAME" && ! test -z "$DB_PASS"; then
-		mysql-create "$DB_NAME" "$DB_PASS"
+		CREATE_DB="CREATE DATABASE `$DB_NAME`; GRANT ALL PRIVILEGES ON `$DB_NAME`.* TO '$DB_NAME'@'localhost' IDENTIFIED BY '$DB_PASS';"
 	fi
+
+	TFILE=`mktemp`
+	cat << EOF > $TFILE
+USE mysql;
+UPDATE user SET password=PASSWORD('$MYSQL_ROOT_PASS') WHERE user='root' AND host='localhost';
+DROP DATABASE test;
+$CREATE_SQL_ADMIN
+$CREATE_DB
+EOF
+
+	/usr/bin/mysqld --bootstrap --verbose=1 --skip-name-resolve < $TFILE
+else
+	/usr/bin/mysqld --bootstrap --verbose=0 --skip-name-resolve
 fi
 
-if test -f /etc/init.d/apache2; then
-	service apache2 start
-fi
-
-if test -f /etc/init.d/ssh; then
-	service ssh start
-fi
-
-if test -f /webhome/docker.sh; then
-	# custom startup
-	/webhome/docker.sh
-fi
+ssh-keygen -A
+/usr/sbin/sshd 
 
 exec "$@"
