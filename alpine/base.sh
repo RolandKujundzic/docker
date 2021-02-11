48778b7c46b8c7842b9b618e8d5736e7f4fed540
Roland Kujundzic <roland@inkoeln.com>
2021-02-11 22:12:20 +0100

    shellcheck

--- alpine/base.sh
+++ alpine/base.sh
@@ -1,2 +1,3 @@
+# shellcheck disable=SC2034
 DOCKER_IMAGE=rk_alpine:base
 DOCKER_DF=base.df
--- alpine/webserver.sh
+++ alpine/webserver.sh
@@ -1,3 +1,4 @@
+# shellcheck disable=SC2034,SC2125
 DOCKER_IMAGE=rk_alpine:webserver
 DOCKER_DF=webserver.df
 DOCROOT_SOURCE=/webhome/***
